cfa9ed303f6d2028cadcbd71f10b32e08694bf45
vagrant <vagrant@shopscript>
2018-11-05 11:01:23 +0000

    Update frontend/tests

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -8,13 +8,14 @@ class HomeCest
 {
     public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnPage('/index.php');
+        $I->amOnPage('/index-test.php');
         $I->canSee('Новинки в магазине');
 //        $I->see('My Application');
 //
         $I->seeLink('Информация');
         $I->click('Информация');
-        $I->amOnPage('/about');
+        $I->amOnPage('/index.php?r=site/about');
+        $I->canSee('Сайт, торгующий товарами в Интернете. Позволяет пользователям сформировать заказ на покупку, выбрать способ оплаты и доставки заказа в сети Интернет. Выбрав необходимые товары или услуги, пользователь обычно имеет возможность тут же на сайте выбрать метод оплаты и доставки.', 'p');
 //        $I->wait(2); // wait for page to be opened
 //
 //        $I->see('This is the About page.');
