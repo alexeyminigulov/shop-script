121758d89c98b3b11f5544cc50258007517c9947
vagrant <vagrant@shopscript>
2018-11-07 12:14:54 +0000

    Update view frontend site/error

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,31 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="tygh-content clearfix">
+    <div class="container-fluid content-grid">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+        <div class="container-fluid-row">
+            <div class="row-fluid ">
+                <div class="span16 breadcrumbs-grid">
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+                    <div class="site-error">
+                        <h1><?= Html::encode($this->title) ?></h1>
+                        <div class="alert alert-danger">
+                            <?= nl2br(Html::encode($message)) ?>
+                        </div>
+                        <p>The above error occurred while the Web server was processing your request.</p>
+                        <p>Please contact us if you think this is a server error. Thank you.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid-row">
+
+            <?= $content ?>
+
+        </div>
+
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
